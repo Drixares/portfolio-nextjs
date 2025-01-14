d9783285159c1c186bafd2e6102db366f1e25bac
Mattéo <matteo.lorenzoq@gmail.com>
2025-01-14 17:31:53 +0100

    chore: added constants variables (PAGES & ROUTES)

---
+++ constants/page.ts
@@ -0,0 +1,25 @@
+export const PAGES = {
+    HOME: "/",
+    PROJECTS: "/projects",
+    EXPERIENCES: "/experiences",
+    WRITING: "/writing",
+};
+
+export const ROUTES = [
+    {
+        label: "Home",
+        path: PAGES.HOME,
+    },
+    {
+        label: "Projects",
+        path: PAGES.PROJECTS,
+    },
+    {
+        label: "Experiences",
+        path: PAGES.EXPERIENCES,
+    },
+    {
+        label: "Writing",
+        path: PAGES.WRITING,
+    },
+];
