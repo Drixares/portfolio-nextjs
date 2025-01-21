fcc663c2adf06c5ed162e61bb91359f06ba7ae1d
Mattéo <matteo.lorenzoq@gmail.com>
2025-01-21 17:09:59 +0100

    fix: updated hoverline animation

--- components/hoverline.tsx
+++ components/hoverline.tsx
@@ -5,14 +5,13 @@ import { motion } from "motion/react";
 const Hoverline = () => {
     return (
         <motion.span
-            className="absolute left-1/2 top-1/2 -z-10 -translate-x-1/2 -translate-y-[40%] 
-                    w-[102%] h-[90%] bg-[#F8DFCB] origin-left -rotate-1"
-            initial={{
-                scaleX: 0,
-                translate: "-50% -40%",
-                rotate: -1,
+            className="absolute left-1/2 top-1/2 -z-10 w-[102%] h-[90%] bg-[#F8DFCB] "
+            style={{
+                transform: "translateX(-50%) translateY(-40%) rotate(-1deg)",
+                transformOrigin: "center left",
             }}
-            animate={{ scaleX: 1 }}
+            initial={{ width: 0 }}
+            animate={{ width: "102%" }}
             transition={{ duration: 0.4, ease: "easeInOut" }}
         />
     );
