30dd3f0f7da4f6c54ee10dcb5d664f9f0b677f30
Mattéo <matteo.lorenzoq@gmail.com>
2025-10-30 16:30:41 +0100

    fix: removed writings page

--- constants/page.ts
+++ constants/page.ts
@@ -1,47 +1,42 @@
 export const PAGES = {
-    HOME: "/",
-    PROJECTS: "/projects",
-    WRITINGS: "/writings",
-    CONTACT: "/contact",
+	HOME: "/",
+	PROJECTS: "/projects",
+	CONTACT: "/contact",
 };
 
 export const ROUTES = [
-    {
-        label: "Home",
-        path: PAGES.HOME,
-    },
-    {
-        label: "Projects",
-        path: PAGES.PROJECTS,
-    },
-    // {
-    //     label: "Writings",
-    //     path: PAGES.WRITINGS,
-    // },
-    {
-        label: "Contact",
-        path: PAGES.CONTACT,
-    },
+	{
+		label: "Home",
+		path: PAGES.HOME,
+	},
+	{
+		label: "Projects",
+		path: PAGES.PROJECTS,
+	},
+	{
+		label: "Contact",
+		path: PAGES.CONTACT,
+	},
 ];
 
 export const NAVLINKS = [
-    {
-        label: "Github",
-        path: "https://github.com/drixares",
-    },
-    {
-        label: "Linkedin",
-        path: "https://www.linkedin.com/in/matt%C3%A9o-marchelli-541b3b224/",
-    },
-    {
-        label: "X",
-        path: "https://x.com/drixares",
-    },
-    {
-        label: "CV",
-        path:
-            process.env.NODE_ENV === "production"
-                ? "https://matteo-marchelli.com/CV.pdf"
-                : "http://localhost:3000/CV.pdf",
-    },
+	{
+		label: "Github",
+		path: "https://github.com/drixares",
+	},
+	{
+		label: "Linkedin",
+		path: "https://www.linkedin.com/in/matt%C3%A9o-marchelli-541b3b224/",
+	},
+	{
+		label: "X",
+		path: "https://x.com/drixares",
+	},
+	{
+		label: "CV",
+		path:
+			process.env.NODE_ENV === "production"
+				? "https://matteo-marchelli.com/CV.pdf"
+				: "http://localhost:3000/CV.pdf",
+	},
 ];
